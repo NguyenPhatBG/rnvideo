51750047d0f692603229fe68627d098e0ab33bd8
NguyenPhatBG <giatocgiausang1992@gmail.com>
2018-09-09 19:58:02 +0700

    Video Drag to Corner
    Create a Youtube Video Page with Animated Video Drag to Corner in React Native

--- App.js
+++ App.js
@@ -1,139 +1,234 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Dimensions, TouchableWithoutFeedback, Animated, PanResponder } from "react-native";
-import Video from "react-native-video";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import ProgressBar from 'react-native-progress/Bar';
-import LightVideo from "./big_buck_bunny.mp4";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  PanResponder,
+  Animated,
+  StatusBar
+} from "react-native";
 
-function secondsToTime(time) {
-  return ~~(time / 60) + ":" + (time % 60 < 10 ? "0" : "") + time % 60;
-}
+import Video from "react-native-video";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import Lights from "./big_buck_bunny.mp4";
+import Thumbnail from "./thumbnail.jpg";
+import ChannelIcon from "./icon.png";
+
+const TouchableIcon = ({ name, children }) => {
+  return (
+    <TouchableOpacity style={styles.touchIcon}>
+      <Icon name={name} size={30} color="#767577" />
+      <Text style={styles.iconText}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const PlaylistVideo = ({ name, channel, views, image }) => {
+  return (
+    <View style={styles.playlistVideo}>
+      <Image source={image} style={styles.playlistThumbnail} resizeMode="cover" />
+      <View style={styles.playlistText}>
+        <Text style={styles.playlistVideoTitle}>
+          {name}
+        </Text>
+        <Text style={styles.playlistSubText}>
+          {channel}
+        </Text>
+        <Text style={styles.playlistSubText}>
+          {views} views
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+console.log();
 
 export default class rnvideo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paused: false,
-      progress: 0,
-      duration: 0
-    };
-  }
-
-  animated = new Animated.Value(0)
-
-  componentWillMount = () => {
-    this.panResponder = PanResponder.create({
-      onMoveShouldSetPanResponderCapture: () => {
-        this.triggerShowHide();
-        return false;
-      }
+  componentWillMount() {
+    this._y = 0;
+    this._animation = new Animated.Value(0);
+    this._animation.addListener(({ value }) => {
+      this._y = value;
     })
-  };
 
-
-  handleLoad = (meta) => {
-    this.triggerShowHide();
-    this.setState({
-      duration: meta.duration
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dy: this._animation,
+        },
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        if (gestureState.dy > 100) {
+          Animated.timing(this._animation, {
+            toValue: 300,
+            duration: 200,
+          }).start();
+          this._animation.setOffset(300);
+        } else {
+          this._animation.setOffset(0);
+          Animated.timing(this._animation, {
+            toValue: 0,
+            duration: 200,
+          }).start();
+        }
+      },
     });
   }
-
-  handleProgress = (progress) => {
-    this.setState({
-      progress: progress.currentTime / this.state.duration
-    });
-  }
-
-  handleEnd = () => {
-    this.setState({ paused: true });
-  }
-
-  handleMainButtonTouch = () => {
-    if (this.state.progress >= 1) {
-      this.player.seek(0);
-    }
-    this.setState(state => {
-      return {
-        paused: !state.paused
-      }
-    })
-  }
-
-  handleProgressPress = (e) => {
-    const position = e.nativeEvent.locationX;
-    const progress = (position / 250) * this.state.duration;
-    this.player.seek(progress);
-  }
-
-  triggerShowHide = () => {
-    clearTimeout(this.hideTimeout);
-    Animated.timing(this.animated, {
-      toValue: 1,
-      duration: 100
+  handleOpen = () => {
+    this._animation.setOffset(0);
+    Animated.timing(this._animation, {
+      toValue: 0,
+      duration: 200,
     }).start();
-    this.hideTimeout = setTimeout(() => {
-      Animated.timing(this.animated, {
-        toValue: 0,
-        duration: 300
-      }).start();
-    }, 1500);
   }
-
   render() {
-    const { width } = Dimensions.get('window');
-    const height = width * .5625;
+    const { width, height: screenHeight  } = Dimensions.get("window");
+    const videoHeight = width * 0.5625;
 
-    const interpolatedControls = this.animated.interpolate({
-      inputRange: [0, 1],
-      outputRange: [48, 0],
-    })
+    const padding = 15;
+    const statusBarHeight = StatusBar.currentHeight || 0;
+    const yOutput = ((screenHeight - videoHeight) + (( videoHeight * .5) / 2)) - padding - statusBarHeight;
+    const xOutput = ((width * .5) / 2) - padding;
 
-    const controlHideStyle = {
+    const opacityInterpolate = this._animation.interpolate({
+      inputRange: [0, 300],
+      outputRange: [1, 0],
+    });
+
+    const translateYInterpolate = this._animation.interpolate({
+      inputRange: [0, 300],
+      outputRange: [0, yOutput],
+      extrapolate: "clamp",
+    });
+
+    const scaleInterpolate = this._animation.interpolate({
+      inputRange: [0, 300],
+      outputRange: [1, 0.5],
+      extrapolate: "clamp",
+    });
+
+    const translateXInterpolate = this._animation.interpolate({
+      inputRange: [0, 300],
+      outputRange: [0, xOutput],
+      extrapolate: "clamp",
+    });
+
+    const scrollStyles = {
+      opacity: opacityInterpolate,
       transform: [
         {
-          translateY: interpolatedControls
-        }
-      ]
-    }
+          translateY: translateYInterpolate,
+        },
+      ],
+    };
+
+    const videoStyles = {
+      transform: [
+        {
+          translateY: translateYInterpolate,
+        },
+        {
+          translateX: translateXInterpolate,
+        },
+        {
+          scale: scaleInterpolate,
+        },
+      ],
+    };
+
     return (
       <View style={styles.container}>
-        <View 
-          {...this.panResponder.panHandlers}
-          style={styles.videoContainer}
-        >
-          <Video
-            paused={this.state.paused}
-            source={LightVideo}
-            repeat={true}
-            style={{ width: '100%', height }}
-            resizeMode='contain'
-            onLoad={this.handleLoad}
-            onProgress={this.handleProgress}
-            onEnd={this.handleEnd}
-            ref={ref => {
-              this.player = ref
-            }}
-          />
-          <Animated.View style={[styles.controls, controlHideStyle]}>
-            <TouchableWithoutFeedback onPress={this.handleMainButtonTouch}>
-              <Icon name={!this.state.paused ? 'pause' : 'play'} size={30} color={'#FFF'} />
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={this.handleProgressPress}>
-              <View>
-                <ProgressBar
-                  progress={this.state.progress}
-                  color={'#FFFFFF'}
-                  unfilledColor='rgba(255,255,255,.5)'
-                  borderColor="#FFFFFF"
-                  width={250}
-                  height={20}
-                />
-              </View>
-            </TouchableWithoutFeedback>
-            <Text style={styles.duration}>
-              {secondsToTime(Math.floor(this.state.progress * this.state.duration))}
-            </Text>
+        <TouchableOpacity onPress={this.handleOpen}>
+          <Text>Content Below: Click To Reopen Video</Text>
+        </TouchableOpacity>
+        <View style={StyleSheet.absoluteFill} pointerEvents="box-none">
+          <Animated.View
+            style={[{ width, height: videoHeight }, videoStyles]}
+            {...this._panResponder.panHandlers}
+          >
+            <Video repeat style={StyleSheet.absoluteFill} source={Lights} resizeMode="contain" />
           </Animated.View>
+          <Animated.ScrollView style={[styles.scrollView, scrollStyles]}>
+            <View style={styles.padding}>
+              <Text style={styles.title}>Beautiful DJ Mixing Lights</Text>
+              <Text>1M Views</Text>
+              <View style={styles.likeRow}>
+                <TouchableIcon name="thumbs-up">10,000</TouchableIcon>
+                <TouchableIcon name="thumbs-down">3</TouchableIcon>
+                <TouchableIcon name="share">Share</TouchableIcon>
+                <TouchableIcon name="download">Save</TouchableIcon>
+                <TouchableIcon name="plus">Add to</TouchableIcon>
+              </View>
+            </View>
+
+            <View style={[styles.channelInfo, styles.padding]}>
+              <Image
+                source={ChannelIcon}
+                style={styles.channelIcon}
+                resizeMode="contain"
+              />
+              <View style={styles.channelText}>
+                <Text style={styles.channelTitle}>Prerecorded MP3s</Text>
+                <Text>1M Subscribers</Text>
+              </View>
+            </View>
+
+            <View style={[styles.playlist, styles.padding]}>
+              <Text style={styles.playlistUpNext}>Up next</Text>
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+              <PlaylistVideo
+                image={Thumbnail}
+                name="Next Sweet DJ Video"
+                channel="Prerecorded MP3s"
+                views="380K"
+              />
+            </View>
+          </Animated.ScrollView>
         </View>
       </View>
     );
@@ -143,29 +238,72 @@ export default class rnvideo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 250
+    alignItems: "center",
+    justifyContent: "center",
   },
-  videoContainer: {
-    overflow: 'hidden',
-    zIndex: 9999 // required on Android
+  scrollView: {
+    flex: 1,
+    backgroundColor: "#FFF",
   },
-  controls: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    height: 48,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    position: 'absolute',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    paddingHorizontal: 10
+  title: {
+    fontSize: 28,
   },
-  mainButton: {
-    marginRight: 15
+  likeRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 15,
   },
-  duration: {
-    color: '#FFF',
-    marginLeft: 15
-  }
+  touchIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconText: {
+    marginTop: 5,
+  },
+  padding: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+  },
+  channelInfo: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDD",
+    borderTopWidth: 1,
+    borderTopColor: "#DDD",
+  },
+  channelIcon: {
+    width: 50,
+    height: 50,
+  },
+  channelText: {
+    marginLeft: 15,
+  },
+  channelTitle: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  playlistUpNext: {
+    fontSize: 24,
+  },
+  playlistVideo: {
+    flexDirection: "row",
+    height: 100,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  playlistThumbnail: {
+    width: null,
+    height: null,
+    flex: 1,
+  },
+  playlistText: {
+    flex: 2,
+    paddingLeft: 15,
+  },
+  playlistVideoTitle: {
+    fontSize: 18,
+  },
+  playlistSubText: {
+    color: "#555",
+  },
 });
